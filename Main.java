7fcc289970178aeaf6e06044a550e556c6114400
Swany122 <92836044+Swany122@users.noreply.github.com>
2021-10-19 21:02:29 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package libreria;
+
+/**
+ * @author Swany Rivera
+ */
+public class Main {
+    public static void main(String[] args) {
+        Login inicio = new Login();
+        inicio.setVisible(true);
+    }
+}
